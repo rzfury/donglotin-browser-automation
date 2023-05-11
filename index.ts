64455071ff42787a49019181ab81cb23b0d8cc86
RZFury <48477044+rzfury@users.noreply.github.com>
2023-05-11 22:42:21 +0700

    disable server

--- index.ts
+++ index.ts
@@ -2,22 +2,20 @@ import { config } from 'dotenv';
 import browserBoot from '~/browser-boot';
 import spawnPage from '~/spawn-page';
 import AppState from '~/utils/state';
-import server from './src-express';
 
 config();
 
 async function app() {
-  server();
   await messaging();
 }
 
 async function messaging() {
   await browserBoot(
     async browser => {
-      await spawnPage();
       setTimeout(() => {
         browser.emit('RZF_DANGER_REBOOT');
       }, 1000 * 60 * 30);
+      await spawnPage();
     }
   );
 
